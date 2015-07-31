7bb20a1bb1ce57f372d3997c1c086c55357629bc
Christopher Schmidt <pecos@sqleb.de>
2015-07-31 02:04:44 +0200

    Added HttpDaemons

---
+++ src/HttpDaemon.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Christopher Schmidt on 30.07.15.
+//
+
+#include "HttpDaemon.h"
---
+++ src/HttpDaemon.h
@@ -0,0 +1,14 @@
+//
+// Created by Christopher Schmidt on 30.07.15.
+//
+
+#ifndef CAMREM_HTTPDAEMON_H
+#define CAMREM_HTTPDAEMON_H
+
+
+class HttpDaemon {
+
+};
+
+
+#endif //CAMREM_HTTPDAEMON_H
