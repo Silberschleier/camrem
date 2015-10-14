c09783af93514d663f94c4938bf5108e4d88b9fd
Christopher Schmidt <pecos@sqleb.de>
2015-10-14 15:09:34 +0200

    Moved GPWrapper to Cam namespace

--- src/Cam/GPWrapper.cpp
+++ src/Cam/GPWrapper.cpp
@@ -4,7 +4,7 @@
 
 #include "GPWrapper.h"
 
-GPWrapper::GPhotoCameraList::GPhotoCameraList() {
+Cam::GPWrapper::GPhotoCameraList::GPhotoCameraList() {
     int ret = gp_list_new( &cameraList_ );
     ok_  = ( GP_OK == ret);
     if ( not ok_ ) {
@@ -12,19 +12,19 @@ GPWrapper::GPhotoCameraList::GPhotoCameraList() {
     }
 }
 
-GPWrapper::GPhotoCameraList::~GPhotoCameraList() {
+Cam::GPWrapper::GPhotoCameraList::~GPhotoCameraList() {
     gp_list_unref( cameraList_ );
 }
 
-bool GPWrapper::GPhotoCameraList::is_valid() {
+bool Cam::GPWrapper::GPhotoCameraList::is_valid() {
     return ok_;
 }
 
-GPWrapper::GPhotoCameraList::operator CameraList *() {
+Cam::GPWrapper::GPhotoCameraList::operator CameraList *() {
     return cameraList_;
 }
 
-GPWrapper::GPhotoAbilitiesList::GPhotoAbilitiesList() {
+Cam::GPWrapper::GPhotoAbilitiesList::GPhotoAbilitiesList() {
     int ret = gp_abilities_list_new( &abilitiesList_ );
     ok_ = ( GP_OK == ret);
     if ( not ok_ ) {
@@ -32,19 +32,19 @@ GPWrapper::GPhotoAbilitiesList::GPhotoAbilitiesList() {
     }
 }
 
-GPWrapper::GPhotoAbilitiesList::~GPhotoAbilitiesList() {
+Cam::GPWrapper::GPhotoAbilitiesList::~GPhotoAbilitiesList() {
     gp_abilities_list_free( abilitiesList_ );
 }
 
-bool GPWrapper::GPhotoAbilitiesList::is_valid() {
+bool Cam::GPWrapper::GPhotoAbilitiesList::is_valid() {
     return ok_;
 }
 
-GPWrapper::GPhotoAbilitiesList::operator CameraAbilitiesList *() {
+Cam::GPWrapper::GPhotoAbilitiesList::operator CameraAbilitiesList *() {
     return abilitiesList_;
 }
 
-GPWrapper::GPhotoPortInfoList::GPhotoPortInfoList() {
+Cam::GPWrapper::GPhotoPortInfoList::GPhotoPortInfoList() {
     int ret = gp_port_info_list_new( &portinfoList_ );
     ok_ = ( GP_OK == ret);
     if ( not ok_ ) {
@@ -52,19 +52,19 @@ GPWrapper::GPhotoPortInfoList::GPhotoPortInfoList() {
     }
 }
 
-GPWrapper::GPhotoPortInfoList::~GPhotoPortInfoList() {
+Cam::GPWrapper::GPhotoPortInfoList::~GPhotoPortInfoList() {
     gp_port_info_list_free( portinfoList_ );
 }
 
-GPWrapper::GPhotoPortInfoList::operator GPPortInfoList *() {
+Cam::GPWrapper::GPhotoPortInfoList::operator GPPortInfoList *() {
     return portinfoList_;
 }
 
-bool GPWrapper::GPhotoPortInfoList::is_valid() {
+bool Cam::GPWrapper::GPhotoPortInfoList::is_valid() {
     return ok_;
 }
 
-GPWrapper::GPhotoCamera::GPhotoCamera() {
+Cam::GPWrapper::GPhotoCamera::GPhotoCamera() {
     int ret = gp_camera_new( &camera_ );
     ok_ = ( GP_OK == ret);
     if ( not ok_ ) {
@@ -72,31 +72,31 @@ GPWrapper::GPhotoCamera::GPhotoCamera() {
     }
 }
 
-GPWrapper::GPhotoCamera::~GPhotoCamera() {
+Cam::GPWrapper::GPhotoCamera::~GPhotoCamera() {
     gp_camera_unref( camera_ );
 }
 
-bool GPWrapper::GPhotoCamera::is_valid() {
+bool Cam::GPWrapper::GPhotoCamera::is_valid() {
     return ok_;
 }
 
-GPWrapper::GPhotoCamera::operator Camera *() {
+Cam::GPWrapper::GPhotoCamera::operator Camera *() {
     return camera_;
 }
 
-GPWrapper::GPhotoContext::GPhotoContext() {
+Cam::GPWrapper::GPhotoContext::GPhotoContext() {
     context_ = gp_context_new();
     ok_ = true;
 }
 
-GPWrapper::GPhotoContext::~GPhotoContext() {
+Cam::GPWrapper::GPhotoContext::~GPhotoContext() {
     gp_context_unref( context_ );
 }
 
-bool GPWrapper::GPhotoContext::is_valid() {
+bool Cam::GPWrapper::GPhotoContext::is_valid() {
     return ok_;
 }
 
-GPWrapper::GPhotoContext::operator GPContext *() {
+Cam::GPWrapper::GPhotoContext::operator GPContext *() {
     return context_;
 }
--- src/Cam/GPWrapper.h
+++ src/Cam/GPWrapper.h
@@ -8,62 +8,66 @@
 #include <gphoto2/gphoto2.h>
 #include "../Helpers.h"
 
-namespace GPWrapper {
-    class GPhotoCameraList {
-    private:
-        bool ok_;
-        CameraList *cameraList_;
-    public:
-        GPhotoCameraList();
-        ~GPhotoCameraList();
-        bool is_valid();
-        operator CameraList *();
-    };
+namespace Cam {
+    namespace GPWrapper {
+        class GPhotoCameraList {
+        private:
+            bool ok_;
+            CameraList *cameraList_;
+        public:
+            GPhotoCameraList();
+            ~GPhotoCameraList();
+            bool is_valid();
+            operator CameraList *();
+        };
 
-    class GPhotoAbilitiesList {
-    private:
-        bool ok_;
-        CameraAbilitiesList *abilitiesList_;
-    public:
-        GPhotoAbilitiesList();
-        ~GPhotoAbilitiesList();
-        bool is_valid();
-        operator CameraAbilitiesList *();
-    };
+        class GPhotoAbilitiesList {
+        private:
+            bool ok_;
+            CameraAbilitiesList *abilitiesList_;
+        public:
+            GPhotoAbilitiesList();
+            ~GPhotoAbilitiesList();
+            bool is_valid();
+            operator CameraAbilitiesList *();
+        };
 
-    class GPhotoPortInfoList {
-    private:
-        bool ok_;
-        GPPortInfoList *portinfoList_;
-    public:
-        GPhotoPortInfoList();
-        ~GPhotoPortInfoList();
-        bool is_valid();
-        operator GPPortInfoList *();
-    };
+        class GPhotoPortInfoList {
+        private:
+            bool ok_;
+            GPPortInfoList *portinfoList_;
+        public:
+            GPhotoPortInfoList();
+            ~GPhotoPortInfoList();
+            bool is_valid();
+            operator GPPortInfoList *();
+        };
 
-    class GPhotoCamera {
-    private:
-        bool ok_;
-        Camera *camera_;
-    public:
-        GPhotoCamera();
-        ~GPhotoCamera();
-        bool is_valid();
-        operator Camera *();
-    };
+        class GPhotoCamera {
+        private:
+            bool ok_;
+            Camera *camera_;
+        public:
+            GPhotoCamera();
+            ~GPhotoCamera();
+            bool is_valid();
+            operator Camera *();
+        };
 
-    class GPhotoContext {
-    private:
-        bool ok_;
-        GPContext *context_;
-    public:
-        GPhotoContext();
-        ~GPhotoContext();
-        bool is_valid();
-        operator GPContext *();
-    };
+        class GPhotoContext {
+        private:
+            bool ok_;
+            GPContext *context_;
+        public:
+            GPhotoContext();
+            ~GPhotoContext();
+            bool is_valid();
+            operator GPContext *();
+        };
+    }
 }
+
+
 
 
 #endif //CAMREM_GPWRAPPER_H
