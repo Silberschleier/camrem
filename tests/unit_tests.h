ea0fe6f846262c1b73f7b07edad7c9e0700cc7b1
Christopher Schmidt <christopher.schmidt@posteo.de>
2015-11-09 22:18:04 +0100

    Configure logging for unit tests

--- tests/unit_tests.h
+++ tests/unit_tests.h
@@ -6,10 +6,14 @@
 #define CAMREM_TESTS_H
 
 #define BOOST_AUTO_TEST_MAIN
+#define ELPP_DISABLE_LOGS
+
 #include <boost/test/auto_unit_test.hpp>
 #include <boost/filesystem.hpp>
 #include "../src/ConfigHandler.h"
 #include "../src/Http/Daemon.h"
 #include "../src/Http/Http.h"
+
+
 
 #endif //CAMREM_TESTS_H
